51c2f416992c3617074390e8d6fd22f68eff0c8a
rezaparamarta <rezaparamarta30@gmail.com>
2025-05-14 19:59:02 +0700

    update

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require("cypress");
+const cucumber = require("cypress-cucumber-preprocessor").default;
 
 module.exports = defineConfig({
   reporter: "cypress-mochawesome-reporter",
